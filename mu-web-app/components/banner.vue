96aa38bc33d759bc5e03dc145477a8f227814e5f
Taki Taki <nhthai2811@gmail.com>
2023-09-07 21:11:41 +0700

    Init product

--- mu-web-app/components/banner.vue
+++ mu-web-app/components/banner.vue
@@ -7,63 +7,16 @@
         </video>
         <div class="logo">
           <img src="/images/logo.png" alt="Banner Image" />
-          <div class="banner-content">
+          <div class="banner-content pt-2">
             <div class="btn-download">
               <img src="images/download.png" alt="Download Image"/>
             </div>
           </div>
-          <div class="banner-content">
+          <div class="banner-content pb-2">
             <h1>CHÀO MỪNG BẠN ĐẾN VỚI CỘNG ĐỒNG LŨ QUỶ</h1>
           </div>
         </div>
       </div>
-    </div>
-    <div class="banner-v2">
-      <div class="banner-v2-header">
-        <div class="banner-v2-content">
-          <div class="d-flex">
-            <div class="down-game">
-              <div class="btn-download">
-                <img src="images/game-info__download.gif" alt="Game info" />
-              </div>
-            </div>
-            <v-carousel class="banner-auto">
-              <v-carousel :cycle="true" :interval="5000" hide-delimiters>
-                <v-carousel-item v-for="(item, index) in items" :key="index">
-                  <div class="center-image">
-                    <img :src="item.image" alt="Banner Image" />
-                  </div>
-                </v-carousel-item>
-              </v-carousel>
-            </v-carousel>
-          </div>
-          <v-list class="list">
-            <h1>TIN TỨC GAME</h1>
-            <v-list-item v-for="(item, index) in linkList" :key="index" link>
-              <v-list-item-title>{{ item.label }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </div>
-        <div class="banner-v2-footer"></div>
-      </div>
-      <!-- <div class="banner-v2-info">
-        <div class="btn-group">
-          <img src="/images/prev.png" alt="Prev button" @click="changeSlide('prev')">
-          <img src="/images/next.png" alt="Next button" @click="changeSlide('next')">
-        </div>
-        <div class="yuanMove"></div>
-        <div class="banner-v2-info-background">
-          <template v-for="(element, index) in bannerList">
-            <div
-              :key="index"
-              class="banner-v2-info-element"
-              :class="{ active: index === currentSlideIndex }"
-            >
-              <img :src="element.image" alt="Banner list image" />
-            </div>
-          </template>
-        </div>
-        </div> -->
     </div>
   </div>
 </template>
@@ -82,93 +35,18 @@ export default class BannerComponent extends Vue {
     {
       image: "images/banner2.png",
     },
-    {
-      image: "images/banner1.png",
-    },
-    {
-      image: "images/banner2.png",
-    },
-    {
-      image: "images/banner1.png",
-    },
-    {
-      image: "images/banner2.png",
-    },
   ];
 
   linkList = [
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
-    {
-      label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
-      link: "#",
-    },
+    // {
+    //   label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
+    //   link: "#",
+    // },
+    // {
+    //   label: "Chuỗi sự kiện hấp dẫn mừng máy chủ mới: MU Hắc Long",
+    //   link: "#",
+    // },
   ];
-
-  bannerList = [
-    {
-      image: "/images/gift05c4ca4.png",
-    },
-    {
-      image: "/images/gift05c4ca4.png",
-    },
-    {
-      image: "/images/gift05c4ca4.png",
-    },
-    {
-      image: "/images/gift05c4ca4.png",
-    },
-    {
-      image: "/images/gift05c4ca4.png",
-    },
-    {
-      image: "/images/gift05c4ca4.png",
-    },
-  ];
-
-  changeSlide(direction) {
-  const scrollContainer = document.querySelector(".banner-v2-info-background");
-  const scrollAmount = 260;
-
-  if (direction === "next") {
-    const maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
-    let newScrollPosition = scrollContainer.scrollLeft + scrollAmount;
-    newScrollPosition = Math.min(newScrollPosition, maxScroll);
-    scrollContainer.scrollTo({
-      left: newScrollPosition,
-      behavior: "smooth",
-    });
-  } else if (direction === "prev") {
-    let newScrollPosition = scrollContainer.scrollLeft - scrollAmount;
-    newScrollPosition = Math.max(newScrollPosition, 0);
-    scrollContainer.scrollTo({
-      left: newScrollPosition,
-      behavior: "smooth",
-    });
-  }
-}
 }
 </script>
 
@@ -186,8 +64,9 @@ export default class BannerComponent extends Vue {
 
     .banner-content {
       width: 100vw;
+      font-size: 16px;
       font-family: "cinzel";
-      padding: 0 100px 30px;
+      padding: 0 100px;
       color: #fff;
       font-weight: bold;
       text-shadow: 0 0 11px #000, 0 0 11px #000, 0 0 11px #000, 0 0 11px #000;
@@ -216,8 +95,8 @@ export default class BannerComponent extends Vue {
     overflow: hidden;
 
     video {
-      width: 100%;
-      height: 800px;
+      width: 100vw;
+      height: calc(100vh - 10px);
       object-fit: cover;
       filter: brightness(0.5);
     }
@@ -225,7 +104,7 @@ export default class BannerComponent extends Vue {
     .logo {
       width: 100vw;
       position: absolute;
-      top: 410px;
+      top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
       z-index: 1;
@@ -236,175 +115,6 @@ export default class BannerComponent extends Vue {
       }
     }
   }
-
-  .banner-v2 {
-    height: 100vh;
-    // min-width: 1600px;
-    display: flex;
-    justify-content: center;
-    padding-top: 100px;
-    min-height: 1000px;
-    margin-top: -5px;
-    background: url("/images/bgjsdesign3.jpg") no-repeat center;
-    background-size: cover;
-
-    .banner-v2-header {
-      background: url("/images/page-main-top.png") no-repeat top;
-      width: 1000px;
-      position: relative;
-      min-height: 430px;
-      margin: 0;
-      padding: 20px 0px 0;
-
-      @media (max-width: 1600px) {
-        transform: scale(0.9);
-      }
-
-      @media (max-width: 1200px) {
-        transform: scale(0.7);
-        top: -200px
-      }
-
-      @media (max-width: 800px) {
-        transform: scale(0.4);
-        top: -300px
-      }
-
-      @media (max-width: 600px) {
-        transform: scale(0.3);
-        top: -300px
-      }
-
-      .banner-v2-content {
-        background: url("/images/page-main-loop.png");
-        padding: 0 5px 0px;
-
-        ::v-deep .v-window {
-          height: 222px !important;
-        }
-
-        .down-game {
-          width: 30%;
-          height: 300px;
-          margin: -81px 10px;
-          background: url(/images/game-info-bg.png);
-          padding: 92px 0 0 24px;
-
-          transition: transform 0.2s ease;
-
-          &:hover {
-            transform: scale(1.05);
-            cursor: pointer;
-          }
-
-          .btn-download {
-            img {
-              height: 189px;
-              width: 262px;
-            }
-          }
-        }
-
-        .banner-auto {
-          width: 693px;
-        }
-      }
-
-      .center-image {
-        display: flex;
-        justify-content: center;
-        padding: 10px;
-
-        img {
-          width: 100%;
-          border: 1px solid #333333;
-        }
-      }
-
-      ::v-deep .v-carousel__controls,
-      ::v-deep .v-window__prev,
-      ::v-deep .v-window__next {
-        display: none;
-      }
-
-      ::v-deep .list {
-        background-color: #1e1e1e00;
-        padding: 0px 15px;
-      }
-    }
-
-    .banner-v2-footer {
-      min-height: 150px;
-      background: url("/images/page-main-bot.png") no-repeat bottom;
-    }
-
-    .banner-v2-info {
-      position: relative;
-      min-width: 600px;
-
-      @media (max-width: 1600px) {
-        transform: scale(0.8);
-      }
-
-      @media (max-width: 1200px) {
-        top: -200px;
-        transform: scale(0.6);
-      }
-
-      @media (max-width: 800px) {
-        top: -200px;
-        transform: scale(0.4);
-      }
-
-      @media (max-width: 600px) {
-        top: -200px;
-        transform: scale(0.3);
-      }
-
-      .banner-v2-info-background {
-        width: 260px;
-        display: flex;
-        position: absolute;
-        overflow: hidden;
-        left: 245px;
-        margin: 165px 0;
-        z-index: 99;
-      }
-
-      .btn-group {
-        position: absolute;
-        display: flex;
-        width: 390px;
-        justify-content: space-between;
-        margin: 330px 177px;
-
-        img {
-          cursor: pointer;
-        }
-      }
-
-      .yuanMove {
-        background: url(/images/yuan.png) no-repeat 0 0;
-        position: absolute;
-        width: 750px;
-        height: 750px;
-        margin: 0 auto;
-        /* Thêm mã CSS cho vòng tròn */
-        animation: spin 15s linear infinite;
-      }
-
-      @keyframes spin {
-        0% {
-          transform: rotate(0deg);
-        }
-        100% {
-          transform: rotate(360deg);
-        }
-      }
-    }
-  }
-
-  
 }
 </style>
   
--- mu-web-app/components/header.vue
+++ mu-web-app/components/header.vue
@@ -1,24 +1,47 @@
 <template>
-    <v-app-bar class="header-custom" app color="rgb(255 0 0 / 82%)">
-        <v-spacer></v-spacer>
-        <v-btn text>Trang chủ</v-btn>
-        <v-btn text>Discord</v-btn>
-        <v-btn text>Fan page</v-btn>
-    </v-app-bar>
+  <v-app-bar class="header-custom" app color="rgb(255 0 0 / 82%)">
+    <v-spacer></v-spacer>
+    <v-btn @click="move('home')" text>Trang chủ</v-btn>
+    <v-btn @click="move('tintuc')" text>Tin tức</v-btn>
+    <v-btn @click="move('discord')" text>Discord</v-btn>
+    <v-btn @click="move('fanpage')" text>Fan page</v-btn>
+  </v-app-bar>
 </template>
 
 <script>
-import { Component } from 'nuxt-property-decorator'
-import Vue from 'vue'
+import { Component } from "nuxt-property-decorator";
+import Vue from "vue";
 
 @Component
 export default class HeaderComponent extends Vue {
+  move(target) {
+    const url = this.getTarget(target)
+    if (target === "discord" || target === "fanpage") {
+      window.open(url, '_blank'); // Mở trong tab mới
+    } else {
+      this.$router.push(url); // Sử dụng router cho các trường hợp khác
+    }
+  }
+
+  getTarget(target) {
+    switch (target) {
+      case "tintuc":
+        return "/notifications";
+      case "discord":
+        return "https://discord.com/invite/Wachty";
+      case "fanpage":
+        return "https://www.facebook.com/groups/379892046811698";
+      default:
+        return "/";
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .header-custom {
   display: flex;
+  overflow: auto;
   justify-content: flex-start;
   color: white;
 }
